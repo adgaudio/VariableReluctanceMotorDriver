ba5df229bed2e236f710297e329c8d34f7652d1b
Alex Gaudio <adgaudio@gmail.com>
2014-07-09 23:10:50 -0400

    node doesn't connect because can't configure baud

--- index.js
+++ index.js
@@ -4,40 +4,33 @@ var baudrate = process.argv[4] || 9600;
 
 console.log(port_fp)
 console.log("--------");
-var ledPin = 13;
 
 var firmata = require('firmata');
 var board = new firmata.Board(port_fp, function(err){
  if (err) {
-        console.log(err);
-        return;
-    }
-  console.log('connected');
-  console.log(
-    'Firmware: '
-    + board.firmware.name
-    + '-' + board.firmware.version.major
-    + '.' + board.firmware.version.minor);
+   console.log('failed');
+   console.log(err);
+   return;
+ }
+ console.log('connected');
+ console.log(
+   'Firmware: '
+   + board.firmware.name
+   + '-' + board.firmware.version.major
+   + '.' + board.firmware.version.minor);
 
+	board.on('string',function(string){
+		console.log(string);
+	});
 
-  var ledOn = true;
-  for (var i = 0; i<32; i++) {
-    board.pinMode(i, board.MODES.OUTPUT);
-  }
-  setInterval(function(){
-    if (ledOn) {
-          console.log('+');
-          for (var i = 0; i<32; i++) {
-            board.digitalWrite(i, board.HIGH);
-          }
-    }
-    else {
-          console.log('-');
-          for (var i = 0; i<32; i++) {
-            board.digitalWrite(i, board.LOW);
-          }
-    }
-
-    ledOn = !ledOn;
+   setInterval(function(){
+       console.log('+');
+       for (var i = 0; i<32; i++) {
+         board.analogWrite(1, 1);
+       }
+   }, 1);
 });
-});
+
+console.log("hi");
+console.log(board.pins);
+
