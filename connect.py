906cc5c2b568b084b87ae5f4ab2e61d84301c24b
Alex Gaudio <adgaudio@gmail.com>
2014-10-01 23:24:37 -0400

    python code to connect to arduino, since nodejs code doesn't seem to work yet

---
+++ connect.py
@@ -0,0 +1,21 @@
+import firmata
+import argparse
+
+
+def main(ns):
+    print("Connecting to %s" % ns.port)
+    board = firmata.Arduino(ns.port, ns.baud)
+    print board  # TODO
+    global board
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    'port', help=("the filepath to the arduino device. "
+                  " ie /dev/tty.SLAB_USBtoUART"))
+parser.add_argument(
+    'baud', help=("baud rate. ie 19200"), type=int)
+
+if __name__ == '__main__':
+    NS = parser.parse_args()
+    main(NS)
